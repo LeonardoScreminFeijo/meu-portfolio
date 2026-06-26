0737cf6e2773421cf3636ba6890f8b6eb75a8e97
LeonardoScreminFeijo <leofeijo2001@hotmail.com>
2026-06-26 07:28:12 -0300

    criação de um TechScroll para exibir tecnologias conhecidas.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/hero";
+import TechScroll from "@/components/TechScroll/TechScroll";
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <TechScroll />
     </main>
   );
 }
