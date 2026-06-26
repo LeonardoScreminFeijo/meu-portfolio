8ead0aefb49983ea174fda9d109a924b6bba20b4
LeonardoScreminFeijo <leofeijo2001@hotmail.com>
2026-06-26 08:10:18 -0300

    correção bug visual mobile.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -6,6 +6,11 @@ import Header from "../components/Header/header";
 import Footer from "../components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL(
